d61fec0481672f97edda100e8f53b82b1c2be306
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-03-28 14:16:28 +0200

    Working with dates and times in Python

---
+++ date_time.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+# print date and time
+print(datetime.now())
+
+# Solutios2 using time.strftime()
+
+from time import gmtime, strftime
+print(strftime("%Y-%m-%d %H:%M:%S"))
+
